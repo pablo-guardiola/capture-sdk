755e4047339d7601d48e7827b466784bc625ecb6
anadelarosa3 <anadelarosa3@gmail.com>
2024-09-09 19:07:22 -0400

    fix files changed - test

--- ci/files_changed.sh
+++ ci/files_changed.sh
@@ -5,10 +5,20 @@
 #
 # Usage: ./ci/files_changed.sh <regex>
 
-set -e
+set -euo pipefail
 
 # Trap to handle unexpected errors and log them
 trap 'echo "An unexpected error occurred during file change check."; echo "check_result=1" >> "$GITHUB_OUTPUT"; exit 1' ERR
+
+# Determine the base ref or fallback to HEAD when running on main
+# TODO uncomment this is a test for trap
+#if [[ -z "${GITHUB_BASE_REF:-}" ]]; then
+#  echo "GITHUB_BASE_REF is empty, likely running on main branch. Using HEAD~1 for comparison."
+#  base_ref="HEAD"
+#else
+#  git fetch origin "$GITHUB_BASE_REF":"$GITHUB_BASE_REF"
+#  base_ref="origin/$GITHUB_BASE_REF"
+#fi
 
 # Check for file changes
 if git rev-parse --abbrev-ref HEAD | grep -q ^main$ || git diff --name-only "origin/$GITHUB_BASE_REF" | grep -E "$1" ; then
